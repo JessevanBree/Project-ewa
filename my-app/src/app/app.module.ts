52897723c2c0b676091feb5e34f5a7d2d79ec2f5
zora <zora@hva.nl>
2019-10-01 13:16:08 +0200

    git test for reset

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditPopUpComponent } from './components/edit-pop-up/edit-pop-up.component';
+import { UploadPopUpComponent } from './components/upload-pop-up/upload-pop-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditPopUpComponent,
+    UploadPopUpComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/components/edit-pop-up/edit-pop-up.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-pop-up',
+  templateUrl: './edit-pop-up.component.html',
+  styleUrls: ['./edit-pop-up.component.css']
+})
+export class EditPopUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
