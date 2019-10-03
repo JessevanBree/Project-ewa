7d6573bb3a31814610853951126b366d328d41bc
JESSE-PC\Jesse van Bree <Jessevanbree@live.nl>
2019-10-04 00:11:43 +0200

    fixed package errors and added modal-test for abdul

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,12 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditPopUpComponent } from './components/edit-pop-up/edit-pop-up.component';
 import { UploadPopUpComponent } from './components/upload-pop-up/upload-pop-up.component';
+import { AdminPanelComponent } from './components/admin/admin-panel/admin-panel.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AdminUsersComponent } from './components/admin/admin-users/admin-users.component';
+import { AdminOrganisationsComponent } from './components/admin/admin-organisations/admin-organisations.component';
+import { AdminDatasetsComponent } from './components/admin/admin-datasets/admin-datasets.component';
+import { AdminDetailComponent } from './components/admin/admin-detail/admin-detail.component';
+import { ModalTestComponent } from './components/modal-test/modal-test.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditPopUpComponent,
-    UploadPopUpComponent
+	EditPopUpComponent,
+	AdminPanelComponent,
+	UploadPopUpComponent,
+	PageNotFoundComponent,
+	AdminUsersComponent,
+	AdminOrganisationsComponent,
+	AdminDatasetsComponent,
+	AdminDetailComponent,
+	ModalTestComponent
   ],
   imports: [
     BrowserModule,
