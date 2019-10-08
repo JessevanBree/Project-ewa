105609b44dde35ecb5dbe344855259119e844aeb
alim3 <alim3@hva.nl>
2019-10-08 14:40:13 +0200

    Fixes to file directory and modified app-routing

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {DatasetOverviewComponent} from "./components/homepage/dataset-overview/dataset-overview.component";
-import {DatasetDetailComponent} from "./components/homepage/dataset-detail/dataset-detail.component";
+import {DatasetOverviewComponent} from './components/homepage/dataset-overview/dataset-overview.component';
+import {DatasetDetailComponent} from './components/homepage/dataset-detail/dataset-detail.component';
 
 
 const routes: Routes = [
   {path: 'homepage', component: DatasetOverviewComponent,
-    children:[
-      {path:':edit', component: DatasetDetailComponent}]},
+    children: [
+      {path: ':edit', component: DatasetDetailComponent}]},
   {path: '', pathMatch: 'full', redirectTo: 'homepage'}
 ];
 
