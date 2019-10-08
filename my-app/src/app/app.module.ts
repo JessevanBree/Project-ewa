3cb5fc5cc17aeabd7f5d1290ae8b8fca930d554f
rosbaca@hva.nl <aris.rosbach@hva.nl>
2019-10-08 11:04:00 +0200

    component Profile

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './Aris/profile/profile.component';
-import { NavBarComponent } from './Aris/nav-bar-Aris/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    NavBarComponent
   ],
   imports: [
     BrowserModule,
