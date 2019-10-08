c704105c01a52e68956040836a803630b7758ddb
alim3 <alim3@hva.nl>
2019-10-08 14:37:39 +0200

    Added homepage component

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {DatasetOverviewComponent} from "./components/homepage/dataset-overview/dataset-overview.component";
-import {DatasetDetailComponent} from "./components/homepage/dataset-detail/dataset-detail.component";
+import {DatasetOverviewComponent} from './components/homepage/dataset-overview/dataset-overview.component';
+import {DatasetDetailComponent} from './components/homepage/dataset-detail/dataset-detail.component';
 
 import {AdminPanelComponent} from './components/admin/admin-panel/admin-panel.component';
 import {AdminDetailComponent} from './components/admin/admin-detail/admin-detail.component';
@@ -20,21 +20,17 @@ const routes: Routes = [
     }]
   },
   {
-    path: '',
-    redirectTo: '/admin',
-    pathMatch: 'full'
-  },
-  {
     path: 'modal',
     component: ModalTestComponent
   },
-  {path: '**', component: PageNotFoundComponent},
   {
     path: 'homepage', component: DatasetOverviewComponent,
     children: [
       {path: ':edit', component: DatasetDetailComponent}]
   },
-  {path: '', pathMatch: 'full', redirectTo: 'homepage'}
+  {path: '', pathMatch: 'full', redirectTo: 'homepage'},
+  {path: '**', component: PageNotFoundComponent},
+
 
 ];
 
