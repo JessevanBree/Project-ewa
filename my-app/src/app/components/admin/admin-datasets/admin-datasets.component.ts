619391fb2d47b22de8fcfdb8a6a4edd61e8b8c2a
JESSE-PC\Jesse van Bree <Jessevanbree@live.nl>
2019-10-14 21:30:00 +0200

    fixed nav and editted style of admin

--- my-app/src/app/components/admin/admin-datasets/admin-datasets.component.ts
+++ my-app/src/app/components/admin/admin-datasets/admin-datasets.component.ts
@@ -12,13 +12,36 @@ import {ADatasetService} from '../../../services/a-dataset.service';
 	styleUrls: ['./admin-datasets.component.css']
 })
 export class AdminDatasetsComponent implements OnInit {
-	datasets: Dataset[] = [];
+	datasets: Dataset[];
+	uploadIsClicked: boolean = false;
+	editIsClicked: boolean = false;
+	selectedDataset: Dataset;
+	private activeIndex;
 
 	constructor(private aDatasetService: ADatasetService) {
-		this.datasets = aDatasetService.getDatasets();
+		this.datasets = [] = aDatasetService.getDatasets();
 	}
 
 	ngOnInit() {
+	}
+
+	//This method gets the event from child component (edit-pop-up) to save the edited dataset
+	saveRequest($event) {
+		this.editIsClicked = false;
+		//Update (save) the dataset in both arrays
+		this.datasets[this.activeIndex] = $event;
+		this.aDatasetService.updateDataset(this.activeIndex, this.aDatasetService.getDatasets()[this.activeIndex]);
+		console.log("Dataset has been saved");
+	}
+
+	//Check if edit button is clicked to open pop-up
+	onEditButtonClick(datasetIndex) {
+		this.activeIndex = datasetIndex;
+
+		//Create a copy of the dataset so it won't immediately change in dataset overview while editing
+		this.selectedDataset = Dataset.trueCopy(this.datasets[this.activeIndex]);
+
+		this.editIsClicked = true;
 	}
 
 }
