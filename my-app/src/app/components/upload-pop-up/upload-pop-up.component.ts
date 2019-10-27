22685ca0e15a54626f8e204da8b742ad6b6d010b
rosbaca@hva.nl <aris.rosbach@hva.nl>
2019-10-27 12:13:59 +0100

    Up to date

--- my-app/src/app/components/upload-pop-up/upload-pop-up.component.ts
+++ my-app/src/app/components/upload-pop-up/upload-pop-up.component.ts
@@ -1,11 +1,11 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {Dataset} from "../../models/dataset";
-import {NgForm} from "@angular/forms";
-import {User} from "../../models/user";
-import {Organisation} from "../../models/organisation";
-import {AUserService} from "../../services/a-user.service";
-import {AOrganisationService} from "../../services/a-organisation.service";
-import {DatasetService} from "../../services/dataset.service";
+import {Dataset} from '../../models/dataset';
+import {NgForm} from '@angular/forms';
+import {User} from '../../models/user';
+import {Organisation} from '../../models/organisation';
+import {AUserService} from '../../services/a-user.service';
+import {AOrganisationService} from '../../services/a-organisation.service';
+import {DatasetService} from '../../services/dataset.service';
 
 // declare var jQuery:any;
 
@@ -16,7 +16,7 @@ import {DatasetService} from "../../services/dataset.service";
 })
 export class UploadPopUpComponent implements OnInit {
 
-  @ViewChild('formElement', {static:false})
+  @ViewChild('formElement', {static: false})
   private detailForm: NgForm;
 
   constructor(private datasetService: DatasetService, private aUserService: AUserService, private aOrganisationService: AOrganisationService) { }
@@ -24,15 +24,16 @@ export class UploadPopUpComponent implements OnInit {
   ngOnInit() {
   }
 
-  //Retreive form data and upload new dataset
-  onSubmit(form: NgForm){
-    let user: User = this.aUserService.genRandomUser();
-    let org: Organisation = this.aOrganisationService.genRandomOrganisation();
+  // Retreive form data and upload new dataset
+  onSubmit(form: NgForm) {
+    const user: User = this.aUserService.genRandomUser();
+    const org: Organisation = this.aOrganisationService.genRandomOrganisation();
 
-    let newDataset = new Dataset(form.value.name, form.value.description, form.value.publicityInput, user, org);
+    // @ts-ignore
+    const newDataset = new Dataset(form.value.name, form.value.description, form.value.publicityInput, user, org);
     console.log(newDataset);
 
-    //Add new dataset to the service
+    // Add new dataset to the service
     this.datasetService.getDatasets().push(newDataset);
 
     // jQuery('#uploadModal').modal('hide');
