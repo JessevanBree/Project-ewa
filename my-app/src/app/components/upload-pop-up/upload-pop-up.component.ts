dcc0b78696b75f741ef8ae27ae853a6ea6e48ab4
alim3 <alim3@hva.nl>
2019-11-01 00:53:21 +0100

    Changes to html and responsiveness of homepage

--- my-app/src/app/components/upload-pop-up/upload-pop-up.component.ts
+++ my-app/src/app/components/upload-pop-up/upload-pop-up.component.ts
@@ -1,9 +1,6 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {Dataset} from '../../models/dataset';
 import {NgForm} from '@angular/forms';
-import {User} from '../../models/user';
 import {Organisation} from '../../models/organisation';
-import {AUserService} from '../../services/a-user.service';
 import {AOrganisationService} from '../../services/a-organisation.service';
 import {DatasetService} from '../../services/dataset.service';
 
@@ -18,7 +15,8 @@ export class UploadPopUpComponent implements OnInit {
 
   @ViewChild('formElement', {static: false})
   private detailForm: NgForm;
-  
+  private records: any[];
+
   constructor(private datasetService: DatasetService, private aOrganisationService: AOrganisationService) { }
 
 
@@ -30,13 +28,13 @@ export class UploadPopUpComponent implements OnInit {
   onSubmit(form: NgForm){
     //let user: User = this.datasetService.genRandomUser();
     let org: Organisation = this.aOrganisationService.genRandomOrganisation();
-    
+
     //let newDataset = new Dataset(form.value.name, form.value.description, form.value.publicityInput, user, org);
     //console.log(newDataset);
 
     //Add new dataset to the service
     //this.datasetService.getDatasets().push(newDataset);
-    
+
     // jQuery('#uploadModal').modal('hide');
     form.resetForm();
   }
@@ -124,8 +122,8 @@ export class UploadPopUpComponent implements OnInit {
 
   //Reset file
   fileReset() {
-    this.csvReader.nativeElement.value = "";
-    this.records = [];
+    //this.csvReader.nativeElement.value = "";
+   // this.records = [];
   }
 
 }
