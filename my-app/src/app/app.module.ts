2e5bb7e7ce63a5adf1b17729b44f7b24d97a7757
alim3 <alim3@hva.nl>
2019-11-11 20:50:51 +0100

    Fixes to package json files and node modules

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {ChartsModule} from 'ng2-charts';
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
