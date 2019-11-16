6a938b3f8a7ab4f04b631711d72c645a3425fafc
Jesse van Bree <jessevanbree@live.nl>
2019-11-16 22:31:21 +0100

    fix to admin route

--- my-app/src/app/services/auth-guard-admin.service.ts
+++ my-app/src/app/services/auth-guard-admin.service.ts
@@ -1,29 +1,29 @@
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
-import {AUserService} from "./fb-user.service";
-import {FbSessionService} from "./session/fb-session.service";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
+import { AUserService } from "./fb-user.service";
+import { FbSessionService } from "./session/fb-session.service";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class AuthGuardAdminService implements CanActivate {
+	private loggedInUserIsAdmin: boolean;
 
-  constructor(private aUserService: AUserService, private router: Router, private sessionService: FbSessionService) { }
+	constructor(private userService: AUserService, private router: Router) { 
+		this.userService.getLoggedInUser().then(user =>
+			this.loggedInUserIsAdmin = user.isAdmin
+		);
+	}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-	let loggedInUser = null;
-	this.aUserService.getLoggedInUser().then((user) => {
-		loggedInUser = user
-		console.log(user)
-	});
-    if(loggedInUser == null || undefined){
-      console.log("Logged in user is null");
-      this.router.navigate(['**']);
-      return false;
-    } else if(loggedInUser.isAdmin == false || undefined){
-      console.log("Logged in user is no admin");
-      this.router.navigate(['**']);
-      return false;
-    } else return true;
-  }
+	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+		if (sessionStorage.key(0) == null || undefined) {
+			console.log("Logged in user is null");
+			this.router.navigate(['**']);
+			return false;
+		} else if (this.loggedInUserIsAdmin == false || undefined) {
+			console.log("Logged in user is no admin");
+			this.router.navigate(['**']);
+			return false;
+		} else return true;
+	}
 }
