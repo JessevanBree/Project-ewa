83e6bdee82ff2bc654643b304bd0b00f7c9a0275
alim3 <alim3@hva.nl>
2019-12-18 18:19:22 +0100

    Changed home folder to landing-page & router redirects to /landing-page on an empty prefix, dataset overview list length only displayed when searching; file additions and fixes

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -36,7 +36,7 @@ import {EditUserPopupComponent} from './components/edit-user-popup/edit-user-pop
 import { FooterComponent } from './components/footer/footer.component';
 import { AdminOrganisationPanelComponent } from './components/admin-organisation-panel/admin-organisation-panel.component';
 import {AddMemberPopupComponent} from "./components/add-member-popup/add-member-popup.component";
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import {LandingPageComponent} from "./components/landing-page/landing-page.component";
 
 @NgModule({
   declarations: [
