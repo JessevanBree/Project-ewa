77c951522b5c89b8550d4bc40f1319fc885dce5e
AbdulZor <57222786+AbdulZor@users.noreply.github.com>
2019-12-30 15:04:50 +0100

    organisation search pipe is fixed, made pure

--- my-app/src/app/components/admin/admin-organisations/admin-organisations.component.ts
+++ my-app/src/app/components/admin/admin-organisations/admin-organisations.component.ts
@@ -9,94 +9,97 @@ import {OrganisationService} from '../../../services/organisation.service';
 import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
-  selector: 'app-admin-organisations',
-  templateUrl: './admin-organisations.component.html',
-  styleUrls: ['./admin-organisations.component.css']
+    selector: 'app-admin-organisations',
+    templateUrl: './admin-organisations.component.html',
+    styleUrls: ['./admin-organisations.component.css']
 })
 export class AdminOrganisationsComponent implements OnInit {
-  organisations: Organisation[];
-  editIsClicked: boolean;
-  createIsClicked: boolean;
-  activeIndex: number;
-  selectedOrganisation: Organisation;
-  searchFilter: String;
-  emptyList: boolean;
+    organisations: Organisation[];
+    editIsClicked: boolean;
+    createIsClicked: boolean;
+    activeIndex: number;
+    selectedOrganisation: Organisation;
+    searchFilter: String;
+    emptyList: boolean;
 
-  constructor(private aOrganisationService: OrganisationService,
-              private router: Router, private activatedRoute: ActivatedRoute) {
-    this.activeIndex = null;
-    this.selectedOrganisation = null;
-    this.editIsClicked = false;
-    this.createIsClicked = false;
-    this.searchFilter = "";
-    this.organisations = [];
-  }
-
-  ngOnInit(): void {
-    // this.organisations = this.aOrganisationService.getOrganisations();
-    this.aOrganisationService.getAllOrganisations().subscribe(
-      (data: Organisation[]) => {
-        console.log(data);
-        this.organisations = data;
-      },
-      error => console.log(error),
-      () => {
-        console.log("Finished retrieving organisations for admin page");
-      }
-    );
-  }
-
-  onEditClick(originalOrganisationIndex: number): void {
-    this.editIsClicked = true;
-    let copyOrganisation = Organisation.trueCopy(this.aOrganisationService.getOrganisation(originalOrganisationIndex));
-    this.activeIndex = originalOrganisationIndex;
-    this.selectedOrganisation = copyOrganisation;
-    this.router.navigate(['editOrganisation'], {
-      relativeTo: this.activatedRoute,
-      queryParams: {id: this.selectedOrganisation.id}
-    });
-  }
-
-  onCreateButtonClick() {
-    this.createIsClicked = true;
-    this.router.navigate(['createOrganisation'], {
-      relativeTo: this.activatedRoute
-    });
-  }
-
-  saveRequest($event): void {
-    this.editIsClicked = false;
-    // this.aOrganisationService.addOrganisation(Organisation.trueCopy($event));
-    this.organisations = this.aOrganisationService.getOrganisations(); 
-    this.router.navigate(['admin']);
-  }
-
-  onDeleteClick(org: Organisation) {
-    if (confirm("Delete organisation: " + org.name)) {
-      this.aOrganisationService.deleteOrganisation(org);
+    constructor(private aOrganisationService: OrganisationService,
+                private router: Router, private activatedRoute: ActivatedRoute) {
+        this.activeIndex = null;
+        this.selectedOrganisation = null;
+        this.editIsClicked = false;
+        this.createIsClicked = false;
+        this.searchFilter = '';
+        this.organisations = [];
     }
-  }
 
-  checkIfListEmpty(): void {
-    if (this.organisations.length == 0) this.emptyList = true;
-    setTimeout(() => {
-      this.emptyList = document.getElementsByClassName("admin-organisation-item").length == 0;
-    }, 5)
-  }
+    ngOnInit(): void {
+        if (this.aOrganisationService.getOrganisations().length != 0) {
+            this.organisations = this.aOrganisationService.getOrganisations();
+            console.log(this.organisations);
+        } else {
+            this.aOrganisationService.getAllOrganisations().subscribe(
+                (data: Organisation[]) => {
+                    console.log(data);
+                    this.organisations = data;
+                },
+                error => console.log(error),
+                () => {
+                    console.log("Finished retrieving organisations for admin page");
+                }
+            );
+        }
+    }
 
-  onClose(){
-    this.editIsClicked = false;
+    onEditClick(originalOrganisationIndex: number): void {
+        this.editIsClicked = true;
+        let copyOrganisation = Organisation.trueCopy(this.aOrganisationService.getOrganisation(originalOrganisationIndex));
+        this.activeIndex = originalOrganisationIndex;
+        this.selectedOrganisation = copyOrganisation;
+        this.router.navigate(['editOrganisation'], {
+            relativeTo: this.activatedRoute,
+            queryParams: {id: this.selectedOrganisation.id}
+        });
+    }
 
-  }
+    onCreateButtonClick() {
+        this.createIsClicked = true;
+        this.router.navigate(['createOrganisation'], {
+            relativeTo: this.activatedRoute
+        });
+    }
 
-  createPopUpIsClosed() {
-    this.createIsClicked = false;
-    this.router.navigate(['admin']);
-  }
+    saveRequest($event): void {
+        this.editIsClicked = false;
+        // this.aOrganisationService.addOrganisation(Organisation.trueCopy($event));
+        this.organisations = this.aOrganisationService.getOrganisations();
+        this.router.navigate(['admin']);
+    }
 
-  editPopUpIsClosed() {
-    this.editIsClicked = false;
-    this.router.navigate(['admin']);
-  }
+    onDeleteClick(org: Organisation) {
+        if (confirm("Delete organisation: " + org.name)) {
+            this.aOrganisationService.deleteOrganisation(org);
+        }
+    }
 
+    checkIfListEmpty(): void {
+        if (this.organisations.length == 0) this.emptyList = true;
+        setTimeout(() => {
+            this.emptyList = document.getElementsByClassName("admin-organisation-item").length == 0;
+        }, 5)
+    }
+
+    onClose() {
+        this.editIsClicked = false;
+
+    }
+
+    createPopUpIsClosed() {
+        this.createIsClicked = false;
+        this.router.navigate(['admin']);
+    }
+
+    editPopUpIsClosed() {
+        this.editIsClicked = false;
+        this.router.navigate(['admin']);
+    }
 }
